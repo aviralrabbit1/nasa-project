8662c35569d6cce89e5288d5ae8c879964ed96b3
aviralrabbit1 <aviral.robin@gmail.com>
2023-07-26 21:03:15 +0530

    speculated basic architecture

---
+++ server/controller/controller.js
@@ -0,0 +1,2 @@
+// controllers will live alongside the routes because they define how we react to specific route
+// code for API and controllers will be under routes folder
---
+++ server/views/views.js
@@ -0,0 +1,2 @@
+// Not required since the project is client-server application
+// There will be separate dedicated frontend in 'client'
