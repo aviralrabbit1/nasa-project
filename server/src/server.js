a11fdd3bbc21542d4ce4acf2f7be2330c3385f73
aviralrabbit1 <aviral.robin@gmail.com>
2023-08-15 03:39:58 +0530

    GET planets

--- server/src/server.js
+++ server/src/server.js
@@ -4,12 +4,21 @@ const http = require('http');
 
 const app = require('./app');
 
+const { loadPlanetsData } = require('./models/planets.model');
+
 const PORT = process.env.PORT || 4000;
 // "start": "PORT =4000 node src/server.js" in package.json
 
 const server = http.createServer(app);
 
-server.listen(PORT, ()=> {
-    console.log(`Listening on ${PORT}...`);
-});
-// app.listen(); // This can also be used
+async function startServer(){
+    await loadPlanetsData(); // can't call await in top level of file, but inside an asycn function
+
+    server.listen(PORT, ()=> {
+        console.log(`Listening on ${PORT}...`);
+    });
+    // app.listen(); // This can also be used
+}
+
+
+startServer();
