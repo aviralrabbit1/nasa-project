160b3e74cdcd92570d113364ec5f6aafc50a3c52
aviralrabbit1 <aviral.robin@gmail.com>
2023-08-14 07:06:16 +0530

    calling planets in frontend request

--- server/src/routes/planets/planets.router.js
+++ server/src/routes/planets/planets.router.js
@@ -1,5 +1,9 @@
 const express = require('express');
 
+// const planetsController = require('./planets.controller');
+const { 
+    getAllPlanets,
+ } = require('./planets.controller');
 // middleware, grouping together all planet routes
 const planetsRouter = express.Router();
 
