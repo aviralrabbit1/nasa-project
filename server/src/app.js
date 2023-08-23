0a939b7a4dc137075ec188750ebb11c510fee3d5
aviralrabbit1 <aviral.robin@gmail.com>
2023-08-24 02:17:16 +0530

    added server path to index

--- server/src/app.js
+++ server/src/app.js
@@ -15,6 +15,10 @@ app.use(express.json()); // middleware can be used here
 app.use(express.static(path.join(__dirname, '..', 'public'))); //exress middleware
 // serve all of our public files using the path.join function
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
+
 app.use(planetsRouter);
 
 module.exports = app;
