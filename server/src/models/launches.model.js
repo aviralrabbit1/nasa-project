35e29e9225ddf163894a0f441798dc4522485e8f
aviralrabbit1 <aviral.robin@gmail.com>
2023-08-26 07:25:05 +0530

    new default launch data

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -4,10 +4,10 @@ let latestFlightNumber = 100;
 
 const launch = {
     flightNumber: 100,
-    mission: 'Keler Exloration X',
-    rocket: 'Explorer IS1',
-    launchDate: new Date('December 27, 2030'),
-    destination: 'Kepler-442 b',
+    mission: 'Kepler Exploration XIII',
+    rocket: 'Explorer IS2',
+    launchDate: new Date('November 29, 2027'),
+    destination: 'Kepler-1652 b',
     customer: ['ISRO', 'NASA'],
     upcoming: true,
     success: true,
